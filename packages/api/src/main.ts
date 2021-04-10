06bc0324ae9127c98b8acaf5b2058a9cf5341a30
shanmugharajk <shanmugharaj.k@outlook.com>
2021-04-10 06:58:02 +0530

    feat(api): enhanced app bootstarp

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,48 @@
+import * as compression from 'compression';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(),
+    { cors: true },
+  );
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('PORT');
+
+  const validationPipe = new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    dismissDefaultMessages: false,
+    validationError: { target: false },
+  });
+
+  app.enable('trust proxy');
+  app.use(helmet());
+  app.use(compression());
+  app.use(morgan('combined'));
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(validationPipe);
+
+  await app.listen(port, () =>
+    console.log(`\n== NestJS powered server is running at ${port}==\n`),
+  );
 }
-bootstrap();
+
+try {
+  bootstrap();
+} catch (error) {
+  console.log(error);
+}
